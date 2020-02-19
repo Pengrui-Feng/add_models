e22bc603251c4c508aa919a2aaaee04f78b63a6e
pengrui <55395901+Pengrui-Feng@users.noreply.github.com>
2020-02-19 13:49:22 -0500

    Update plot_for_report.py

--- plot_for_report.py
+++ plot_for_report.py
@@ -34,8 +34,9 @@ t_ids=[118940, 118941, 118944, 118947, 118948, 118951, 118943, 118945,118946, 11
        161426, 161427, 161428, 161432, 161433, 161435, 161429, 161436,161437, 161430, 161434, 161439, 161431, 161438, 161440, #tu_99
        161291, 161292, 161296, 172191, 175934, 175935, 161295, 175939,161299, 161303, 161294, 161297, 161298, 161300, 161301, 161304,172179, 172188, 175938, 175932, 175936, 175940] #tu_102
 
-#data = pd.read_csv(path2+db+'withModelsold.csv')
-data = pd.read_csv(path2+'tu102withfModels.csv')
+data = pd.read_csv(path2+'special_data.csv')
+#data = pd.read_csv(path2+'tu102withfModels.csv')
+data['gps_date']=pd.to_datetime(data['gps_date'])
 data = data.dropna()
 #dive = data['dive_num']
 data['obs_temp']=data['obs_temp']*1.8+30
@@ -48,14 +49,14 @@ data['FVCOM_temp']= data['FVCOM_temp'].astype('str')
 data['depth']= data['depth'].astype('str')
 
 #data1=data.groupby(['PTT','dive_num','argos_date','lat_argos','lon_argos','gps_date','lat_gps','lon_gps']).agg(lambda x:x.str.cat(sep=','))
-data1=data.groupby(['PTT','argos_date'])['depth','obs_temp','doppio_temp','FVCOM_temp'].agg(lambda x:x.str.cat(sep=','))
+data1=data.groupby(['PTT','gps_date'])['depth','obs_temp','doppio_temp','FVCOM_temp'].agg(lambda x:x.str.cat(sep=','))
 #data1.to_csv('tu102_adapt.csv')
 data1.to_csv('special_combine.csv')
 obsData =pd.read_csv('special_combine.csv') #
 obsturtle_id=obsData['PTT']
 ids=obsturtle_id.unique()#118905 # this is the interest turtle id
 
-Time = pd.Series((datetime.strptime(x, '%Y-%m-%d %H:%M:%S') for x in obsData['argos_date']))
+Time = pd.Series((datetime.strptime(x, '%Y-%m-%d %H:%M:%S') for x in obsData['gps_date']))
 #indx=np.where((Time>=end_time-timedelta(days=7)) & (Time<end_time))[0]
 indx=np.where((Time>=start_time))[0]
 time=Time[indx]
@@ -63,7 +64,7 @@ time.sort()
 
 Data = obsData.ix[time.index[0:8]]
 Data.index=range(8)
-obsTime =  pd.Series((datetime.strptime(x, '%Y-%m-%d %H:%M:%S') for x in Data['argos_date']))
+obsTime =  pd.Series((datetime.strptime(x, '%Y-%m-%d %H:%M:%S') for x in Data['gps_date']))
 obsTemp = pd.Series(str2ndlist(Data['obs_temp']))
 dopTemp = pd.Series(str2ndlist(Data['doppio_temp']))
 fvcTemp = pd.Series(str2ndlist(Data['FVCOM_temp']))
@@ -92,16 +93,17 @@ for n in range(8):
                 ax.plot(np.array(dopTemp[j]),obsDepth[j],linestyle='--',color=color[i],linewidth=3)
                 ax.plot(np.array(fvcTemp[j]),obsDepth[j],linestyle=':',color=color[i],linewidth=3)        
                 
-                ax.text(obsTemp[j][-1]-6,obsDepth[j][-1]+3,round(obsTemp[j][-1],1),color='r',fontsize=10)                
-                ax.text(dopTemp[j][-1]+1,obsDepth[j][-1]+3,round(dopTemp[j][-1],1),color='g',fontsize=10)                              
+                ax.text(obsTemp[j][-1]-6,obsDepth[j][-1]+3,int(obsTemp[j][-1]),color='r',fontsize=10)                
+                ax.text(dopTemp[j][-1]+1,obsDepth[j][-1]+10,int(dopTemp[j][-1]),color='g',fontsize=10)                              
+                ax.text(fvcTemp[j][-1]+1,obsDepth[j][-1]+3,int(fvcTemp[j][-1]),color='b',fontsize=10)
                 
-                ax.text(obsTemp[j][0]-8,obsDepth[j][0]-1,round(obsTemp[j][0],1),color='r',fontsize=10)
-                ax.text(dopTemp[j][0]-2,obsDepth[j][0]-1,round(dopTemp[j][0],1),color='g',fontsize=10)
-
+                ax.text(obsTemp[j][0]-3,obsDepth[j][0]-4,int(obsTemp[j][0]),color='r',fontsize=10)
+                ax.text(dopTemp[j][0]+1,obsDepth[j][0]-4,int(dopTemp[j][0]),color='g',fontsize=10)
+                ax.text(fvcTemp[j][0]-9,obsDepth[j][0]-4,int(fvcTemp[j][0]),color='b',fontsize=10)
     time=obsTime[n].strftime('%m-%d-%Y')           
-    ax.set_ylim([maxdepth,-10])
-    ax.set_xticks([int(obsTemp[n][-1]-6), int(dopTemp[n][0])+5])
-    ax.text(57, 195,'%s'%time,ha='center', va='center',color='black',fontsize=10)
+    ax.set_ylim([maxdepth+10,-10])
+    ax.set_xticks([int(obsTemp[n][-1]-6), int(dopTemp[n][0])+8])
+    ax.text(57, 205,'%s'%time,ha='center', va='center',color='black',fontsize=10)
     plt.setp(ax.get_xticklabels() ,visible=False)
     if n!=0 and n!=4:
         plt.setp(ax.get_yticklabels() ,visible=False)
@@ -117,6 +119,6 @@ fig.text(0.5, 0.05, 'Turtle(red_label) & Model(green_label) Temperature(degF)', 
 fig.text(0.5, 0.95, 'Turtle(solid) & Model(dash) Profile', ha='center', va='center', fontsize=14)
 fig.text(0.06, 0.5, 'Depth(Ft)', ha='center', va='center', rotation='vertical',fontsize=14)
 plt.subplots_adjust(wspace =0, hspace =0.05)
-#plt.savefig(path2+'/model_compare/compare_profile.png',dpi=200)#put the picture to the file"turtle_comparison"
+plt.savefig(path2+'/model_compare/compare_profile.png',dpi=200)#put the picture to the file"turtle_comparison"
 plt.show()
 
