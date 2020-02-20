a9a8ad5f93755e01721b50e247f1bf0bb105b150
pengrui <55395901+Pengrui-Feng@users.noreply.github.com>
2020-02-20 10:43:47 -0500

    Update add_modules_to_turtles.py

--- add_modules_to_turtles.py
+++ add_modules_to_turtles.py
@@ -11,44 +11,40 @@ import numpy as np
 import pandas as pd
 import zlconversions as zl
 import doppio_model as dm
-#import FVCOM_modules as fm
-#import gomofs_modules as gm
+import fvcom_model as fm
+from datetime import datetime as dt
 
 db= 'tu102' #tu73,tu74,tu94,tu98,tu99,tu102
 path1='/home/zdong/PENGRUI/merge_split/'
 
-Data = pd.read_csv(path1+db+'_merge_split.csv')
-Data['argos_date'] = pd.to_datetime(Data['argos_date'])
-date = Data['argos_date']
+Data = pd.read_csv('special_data.csv')
+#Data = pd.read_csv(path1+db+'_merge_split.csv')#Pengrui
+date = pd.to_datetime(Data['gps_date'])
 lat = Data['lat_gps']
 lon = Data['lon_gps']
+date1 = pd.to_datetime(Data['argos_date'])
 lat1 = Data['lat_argos']
 lon1 = Data['lon_argos']
 depth = Data['depth']
-temp = Data['temp']
+#temp = Data['temp']
 
 doppio_temp = []
 FVCOM_temp = []
-gomofs_temp = []
 
+
+#for i in tqdm(range(27,29)):
 for i in tqdm(range(len(Data))):
     if pd.isnull(lat[i]) or pd.isnull(lon[i]):
-        lat[i],lon[i]=lat1[i],lon1[i]
+        date[i],lat[i],lon[i]=date1[i],lat1[i],lon1[i]
     d_temp=dm.get_doppio(lat[i],lon[i],depth=depth[i],time=str(date[i]))
-    #b=gm.get_gomofs(date[i],lat[i],lon[i],fortype='temperature')
-    #c=fm.get_FVCOM_temp(lat[i],lon[i],date[i],depth='bottom',mindistance=2,fortype='temperature')
-    #print (d_temp)
-    #print (b)
-    #print (c)
-    doppio_temp.append(round(d_temp,3))
-    #FVCOM_temp.append(b)
-    #gomofs_temp.append(c)
-    #print(doppio_temp)
-  
+    f_temp=fm.get_FVCOM_temp(lat[i],lon[i],dtime=str(date[i]),depth=depth[i])
+    #print (depth[i],d_temp)
+    #print (depth[i],f_temp)
+    doppio_temp.append(d_temp)
+    FVCOM_temp.append(f_temp)
 
 Data['doppio_temp']=pd.DataFrame(doppio_temp)
-#c['FVCOM_temp']=pd.DataFrame(FVCOM_temp)
-#c['gomofs_temp']=pd.DataFrame(gomofs_temp)
+Data['FVCOM_temp']=pd.DataFrame(FVCOM_temp)
 Data.rename(index=str, columns={"temp": "obs_temp"},inplace=True)
-Data=Data[['dive_num','PTT','argos_date','lat_argos','lon_argos','gps_date','lat_gps','lon_gps','depth','obs_temp','doppio_temp']]
+Data=Data[['PTT','argos_date','lat_argos','lon_argos','gps_date','lat_gps','lon_gps','depth','obs_temp','doppio_temp','FVCOM_temp']]
 Data.to_csv(db+'withModels.csv')
