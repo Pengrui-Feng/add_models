ad252c9d0829fd044daa73ff4bb6f3a5d721b20b
pengrui <55395901+Pengrui-Feng@users.noreply.github.com>
2020-04-21 20:58:50 -0400

    Update get_espresso_model.py

--- get_espresso_model.py
+++ get_espresso_model.py
@@ -1,3 +1,7 @@
+'''
+espresso_model is previous edition of doppio model
+'''
+#!pip install netCDF4
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
@@ -5,6 +9,12 @@ import netCDF4
 
 def get_espresso_temp(time,lat,lon,depth) :    
     #according to doppio model structure , data is from 2009-10-12 to 2017-1-1
+    time=pd.to_datetime(time)
+    if (time -datetime(2009,10,12)).total_seconds()<0 :
+        #print('the date can\'t be earlier than 2017-11-1')
+        return np.nan
+    if (time -datetime(2017,1,1)).total_seconds()>0:
+        return np.nan
     url=get_url(time)
     nc=netCDF4.Dataset(url).variables
     #first find the index of the grid 
@@ -24,18 +34,11 @@ def get_espresso_temp(time,lat,lon,depth) :
     return espresso_temp
 
 def get_url(time):
-    # hours = int((endtime-starttime).total_seconds()/60/60) # get total hours
-    # time_r = datetime(year=2006,month=1,day=9,hour=1,minute=0)
     if (time- datetime(2013,5,18)).total_seconds()/3600>25:
-        #url_oceantime = 'http://tds.marine.rutgers.edu:8080/thredds/dodsC/roms/espresso/hidden/2006_da/his?ocean_time'
-        #url_oceantime = 'http://tds.marine.rutgers.edu:8080/thredds/dodsC/roms/espresso/2013_da/his_Best/ESPRESSO_Real-Time_v2_History_Best_Available_best.ncd?time'
         url_oceantime = 'http://tds.marine.rutgers.edu/thredds/dodsC/roms/espresso/2013_da/his/ESPRESSO_Real-Time_v2_History_Best?time[0:1:31931]'
         oceantime = netCDF4.Dataset(url_oceantime).variables['time'][:]    #if url2006, ocean_time.
         t1 = (time - datetime(2013,5,18)).total_seconds()/3600 # for url2006 it's 2006,01,01; for url2013, it's 2013,05,18, and needed to be devide with 3600
         index1 = closest_num(t1, oceantime)
-        # url = 'http://tds.marine.rutgers.edu:8080/thredds/dodsC/roms/espresso/2006_da/his?h[0:1:81][0:1:129],s_rho[0:1:35],lon_rho[0:1:81][0:1:129],lat_rho[0:1:81][0:1:129],mask_rho[0:1:81][0:1:129],u[{0}:1:{1}][0:1:35][0:1:81][0:1:128],v[{0}:1:{1}][0:1:35][0:1:80][0:1:129]'
-        #url = 'http://tds.marine.rutgers.edu:8080/thredds/dodsC/roms/espresso/hidden/2006_da/his?s_rho[0:1:35],h[0:1:81][0:1:129],lon_rho[0:1:81][0:1:129],lat_rho[0:1:81][0:1:129],temp[{0}:1:{1}][0:1:35][0:1:81][0:1:129],ocean_time'
-        #url = 'http://tds.marine.rutgers.edu:8080/thredds/dodsC/roms/espresso/2013_da/his_Best/ESPRESSO_Real-Time_v2_History_Best_Available_best.ncd?h[0:1:81][0:1:129],s_rho[0:1:35],lon_rho[0:1:81][0:1:129],lat_rho[0:1:81][0:1:129],temp[{0}:1:{1}][0:1:35][0:1:81][0:1:129],time' 
         url = 'http://tds.marine.rutgers.edu/thredds/dodsC/roms/espresso/2013_da/his/ESPRESSO_Real-Time_v2_History_Best?s_rho[0:1:35],lon_rho[0:1:81][0:1:129],lat_rho[0:1:81][0:1:129],time[0:1:32387],h[0:1:81][0:1:129],temp[0:1:32387][0:1:35][0:1:81][0:1:129]'
         url = url.format(index1)
     else :
